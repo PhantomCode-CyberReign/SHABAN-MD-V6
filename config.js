f8e710c4a2270084b1febecf0fd831b1960165c4
Phantom Code | Cyber Reign <ahadpythonprogrammer@gmail.com>
2025-07-13 13:11:41 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~gIQmGDSC#MUd-7iU799-pK-fs8Jyez7qrR8d6F2eJRSWTmLsyIWs",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
